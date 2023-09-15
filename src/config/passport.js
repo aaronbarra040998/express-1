531070e4588db1a48f6cc32faa3e1a99bcd7360a
aaronbarra040998 <aaron.apaza@tecsup.edu.pe>
2023-09-15 15:54:46 -0500

    expres primer

---
+++ src/config/passport.js
@@ -0,0 +1,72 @@
+const LocalStrategy =require ('passport-local').Strategy;
+
+const User=require('../app/models/user');
+
+module.exports = function(passport) {
+
+    passport.serializeUser(function (user, done) {
+        done(null, user.id);
+      });
+
+
+      passport.deserializeUser(function (id, done) {
+        User.findById(id)
+          .then(function (user) {
+            done(null, user);
+          })
+          .catch(function (err) {
+            done(err, null);
+          });
+      });
+//signup
+passport.use('local-signup', new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'password',
+  passReqToCallback: true,
+},
+async function (req, email, password, done) {
+  try {
+    const user = await User.findOne({ 'local.email': email });
+
+    if (user) {
+      return done(null, false, req.flash('signupMessage', 'The email is already taken'));
+    } else {
+      const newUser = new User();
+      newUser.local.email = email;
+      newUser.local.password = newUser.generateHash(password);
+
+      await newUser.save();
+
+      return done(null, newUser);
+    }
+  } catch (err) {
+    return done(err);
+  }
+}));
+
+
+   //login
+passport.use('local-login', new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'password',
+  passReqToCallback: true
+},
+function (req, email, password, done) {
+  User.findOne({'local.email': email})
+    .exec() // Utiliza .exec() para manejar la consulta
+    .then((user) => {
+      if (!user) {
+        return done(null, false, req.flash('loginMessage', 'No User found'));
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, req.flash('loginMessage', 'Wrong password'));
+      }
+      return done(null, user);
+    })
+    .catch((err) => {
+      return done(err);
+    });
+}));
+
+    }
+    
---
+++ src/server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const app = express();
+
+const path = require('path');
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+const flash = require('connect-flash');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+
+const { url } = require('./config/database.js');
+
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+ require('./config/passport')(passport);
+
+
+const db = mongoose.connection;
+
+// Manejador de eventos para verificar la conexión
+db.on('error', (err) => {
+  console.error('Error de conexión a MongoDB:', err);
+});
+
+db.once('open', () => {
+  console.log('Conexión exitosa a MongoDB');
+  // Iniciar tu servidor Express aquí, si lo deseas
+});
+
+// Resto de tu código aquí...
+
+//settings
+app.set('port', process.env.PORT || 3000);
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','ejs');
+//middleware
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+    secret:'faztwebtutorialnode',
+    resave:false,
+    saveUninitialized:false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+//routes 
+require('./app/routes.js')(app,passport);
+
+//static files
+app.use(express.static(path.join(__dirname,'public')));
+app.listen(app.get('port'), () => {
+  console.log('Server on port ', app.get('port'));
+});
